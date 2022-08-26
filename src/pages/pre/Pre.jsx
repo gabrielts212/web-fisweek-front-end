2a1beecb5fa531c7a9afc47e3192e0a1f0b385c1
Paulo Cereigido <paulocereigido@gmail.com>
2022-08-26 16:47:49 -0300

    adding components

--- src/pages/pre/Pre.jsx
+++ src/pages/pre/Pre.jsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import { Footer } from '../../components/footer/Footer';
+import { Newsletter } from '../../components/newsletter/Newsletter';
+
+import { SaveTheDate } from '../../components/savethedate/SaveTheDate';
+
 
 export function Pre() {
     return (
         <div>
             <h2>Pre</h2>
+            <SaveTheDate />
+            <Newsletter />
             <Link to="/home">Home</Link>
+            <Footer />
         </div>
     );
 }
