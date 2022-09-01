fa03d75f7633fe8cc10ff578332ec5bd373c5e20
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2022-09-01 17:03:14 -0300

    Social networks form created

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -2,6 +2,14 @@ import Image2 from '../../assets/images/image2.png';
 import GeometricShapes4 from '../../assets/images/geometric-shapes4.png';
 import GeometricShapes5 from '../../assets/images/geometric-shapes5.png';
 import GeometricShapes6 from '../../assets/images/geometric-shapes6.png';
+import Checked from '../../assets/images/checked.png';
+import Youtube from '../../assets/images/youtube.png';
+import Spotify from '../../assets/images/spotify.png';
+import Instagram from '../../assets/images/instagram.png';
+import Linkedin from '../../assets/images/linkedin.png';
+import Twitter from '../../assets/images/twitter.png';
+import TikTok from '../../assets/images/tik-tok.png';
+import Facebook from '../../assets/images/facebook.png';
 
 import './Newsletter.css';
 
@@ -26,6 +34,38 @@ export function Newsletter() {
                 <input type="email" name="email" placeholder="Email" required />
                 <button type="submit">CADASTRE-SE</button>
             </form>
+
+            {/* <form className="form-send" action="POST">
+                <img src={Checked} alt="Checked" />
+                <h3 className="h3">Obrigado!</h3>
+                <h4>Se conecte com a gente</h4>
+                <div className="icons-form">
+                    <a href="#">
+                        <img src={Youtube} alt="Youtube" />
+                    </a>
+                    <a href="#">
+                        <img src={Spotify} alt="Spotify" />
+                    </a>
+                    <a href="#">
+                        <img src={Instagram} alt="Instagram" />
+                    </a>
+                    <a href="#">
+                        <img src={Linkedin} alt="linkedin" />
+                    </a>
+                    <a href="#">
+                        <img src={Twitter} alt="Twitter" />
+                    </a>
+                    <a href="#">
+                        <img src={TikTok} alt="TikTok" />
+                    </a>
+                    <a href="#">
+                        <img src={Facebook} alt="Facebook" />
+                    </a>
+                </div>
+                <div className="button">
+                <button type="submit">INSCREVA-SE NO TELEGRAM</button>
+                </div>
+            </form> */}
 
             <img className="geometric-shapes4" src={GeometricShapes4} alt="Forma Geomêtrica" />
             <img className="geometric-shapes5" src={GeometricShapes5} alt="Forma Geomêtrica" />
