9297c48f3163f691b2e816c09e05f0742a53ccc2
Gabriel <91156517+gabrielts212@users.noreply.github.com>
2022-09-13 18:27:04 -0300

    ending non-responsive footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,81 +3,101 @@ import "./Footer.css";
 export function Footer() {
   return (
     <div className="main-footer">
-      <div className="container">
-        {/* //footer */}
-        <div className="contact">
-          <h4>Precisa de ajuda?</h4>
-          <p>faleconosco@fis.org.br</p>
-          <h4>Comercial</h4>
-          <p>faleconosco@fis.org.br</p>
-          <h4>Assessoria de Imprensa</h4>
-          <p>faleconosco@fis.org.br</p>
-        </div>
+        <div className="container">
 
-        <div className="networks">
-          <h4>Nossas redes</h4>
-        </div>
-        <div className="sociais">
-
-          <div className="social">
-            <a href="#"alt="facebook">
-              <i className="fa fa-facebook"></i>
-            </a>
+          <div className="contact">
+            <h4>Precisa de ajuda?</h4>
+            <p>faleconosco@fis.org.br</p>
+            <h4>Comercial</h4>
+            <p>comercial@fis.org.br</p>
+            <h4>Assessoria de Imprensa</h4>
+            <p>faleconosco@fis.org.br</p>
           </div>
-
-          <div className="social">
-            <a href="#"alt="instagram">
+  
+          <div className="networks">
+            <h4>Nossas redes</h4>
+          </div>
+          
+          <div className="sociais">
+  
+            <div className="social">
+              <a href="#"alt="facebook">
+                <i className="fa fa-facebook"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="instagram">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="instagram">
               <i className="fa fa-instagram"></i>
-            </a>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="twitter">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="youtube">
+                <i className="fa fa-youtube"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="linkedin">
+                <i className="fa fa-linkedin"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="spotify">
+                <i className="fa fa-spotify"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="telegram">
+                <i className="fa fa-telegram"></i>
+              </a>
+            </div>
+  
+            <div className="social">
+              <a href="#"alt="tiktok">
+              <i className="fa-brands fa-tiktok"></i>
+              </a>
+            </div>
+          </div>
+          <div className="text-fis">
+            <p>POWERED BY </p>
+          </div>
+          <div className="text-fis2">
+            <h4>INICIATIVAFIS</h4>
           </div>
 
-          <div className="social">
-            <a href="#"alt="instagram">
-              <i className="fa fa-instagram"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="twitter">
-              <i className="fa fa-twitter"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="youtube">
-              <i className="fa fa-youtube"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="linkedin">
-              <i className="fa fa-linkedin"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="spotify">
-              <i className="fa fa-spotify"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="telegram">
-              <i className="fa fa-telegram"></i>
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="#"alt="tiktok">
-              <i class="fa-brands fa-tiktok"></i>
-            </a>
-          </div>
         </div>
-        <div className="text-fis">
-          <p>POWERED BY <h5>INICIATIVAFIS</h5></p>
-        </div>
-        {/* footer end */}
       </div>
-    </div>
   );
 }
+
+
+
+
+
+
+{/* <h4>Comercial</h4>
+            <ul><li>
+              <p>faleconosco@fis.org.br</p>
+              </li></ul>
+
+              <h4>Assessoria de Imprensa</h4>
+          <ul><li> 
+          <p>faleconosco@fis.org.br</p>
+            </li></ul>
+        </div> */}
