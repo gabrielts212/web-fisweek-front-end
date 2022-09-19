c52745fa48c02844aedd129ed36d188b770c8cc3
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2022-09-19 13:40:35 -0300

    Adjusting the space-between in the footer

--- src/components/programming/Programming.jsx
+++ src/components/programming/Programming.jsx
@@ -2,6 +2,29 @@ import './Programming.css'
 
 export function Programming() {
   return (
-    <h1>Hello!</h1>
+    <div className="Programming">
+      <header>
+        <h1>Programação</h1>
+      </header>
+      <section className="daysOfTheWeek">
+        <div className="Days">Todos</div>
+        <div className="Days">Dia 7</div>
+        <div className="Days">Dia 8</div>
+        <div className="Days">Dia 9</div>
+        <div className="Days">Dia 10</div>
+        <div className="Days">Dia 11</div>
+      </section>
+
+      <section className="dates">
+        <div className="date"></div>
+        <div className="dateAndTime"></div>
+      </section>
+
+      <section className="Speakers">
+        <p>Os desafios de implementar políticas públicas de saúde durante uma pandemia</p>
+        <img src={PanelLogo} alt="Logo do painel" />
+        
+      </section>
+    </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,20 +15,20 @@ export function Home() {
     return (
         <Container>
             <Row>
-                <Col xs={12} md={8}><RegistrationInfo /></Col>
-                <Col xs={12} md={4}><ApplicationForm /></Col>
+                {/* <Col xs={12} md={8}><RegistrationInfo /></Col>
+                <Col xs={12} md={4}><ApplicationForm /></Col> */}
             </Row>
             <Row>
                 <ConferenceBar />
             </Row>
             <Row>
-                <Leaders/>
+                {/* <Leaders/> */}
             </Row>
             <Row>
                 <Programming />
             </Row>
             <Row>
-                <Footer />
+                {/* <Footer /> */}
             </Row>
         </Container>
     );
