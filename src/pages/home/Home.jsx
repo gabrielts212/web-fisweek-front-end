7888ffd79a9b915789557e6ae480e0e30ecb8049
Gabriel <91156517+gabrielts212@users.noreply.github.com>
2022-09-19 18:46:03 -0300

    leaders images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,8 +14,8 @@ export function Home() {
     return (
         <Container>
             <Row>
-                <Col xs={12} md={8}><RegistrationInfo /></Col>
-                <Col xs={12} md={4}><ApplicationForm /></Col>
+                {/* <Col xs={12} md={8}><RegistrationInfo /></Col>
+                <Col xs={12} md={4}><ApplicationForm /></Col> */}
             </Row>
             <Row>
                 <ConferenceBar />
@@ -26,7 +26,7 @@ export function Home() {
             </Row>
 
             <Row>
-                <Footer />
+                {/* <Footer /> */}
             </Row>
         </Container>
     );
