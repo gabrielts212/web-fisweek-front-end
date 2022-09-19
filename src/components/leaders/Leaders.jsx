4df4d4a75c151a6a611e88b40587fd37401cad21
Gabriel <91156517+gabrielts212@users.noreply.github.com>
2022-09-19 13:39:41 -0300

    leaders modifications 2

--- src/components/leaders/Leaders.jsx
+++ src/components/leaders/Leaders.jsx
@@ -1,18 +1,38 @@
-import './Leaders.css'
-import Leader1 from '../../assets/images/leaders/leader1.png';
-import Rectangle from '../../assets/images/';
+import "./Leaders.css";
+import Leader1 from "../../assets/images/leaders/leader1.png";
+import Rectangle from "../../assets/images/leaders/Rectangle.png";
+import Rectangle2 from "../../assets/images/leaders/Rectangle2.png";
 
 export function Leaders() {
-    return (  
+  return (
+    <div className="Leaders">
 
-<div className='Leaders'>
-    <h1>AQUI</h1>
-    <img className="Leader1" src={Leader1} alt="image-leader" />
-    <img className="Leader1" src={Leader1} alt="image-leader" />
-    <img className="Leader1" src={Leader1} alt="image-leader" />
+      <div className="images-leaders">
+        <img className="Leader1" src={Leader1} alt="imagem-lider" />
+        <img className="Leader1" src={Leader1} alt="imagem-lider" />
+      </div>
 
-</div>
+      <div className="images-leaders2">
+      </div>
 
-    );
+      <div className="images-leaders3">
+        <img className="Leader1" src={Leader1} alt="imagem-lider" />
+      </div>
+
+      <img className="Rectangle" src={Rectangle} alt="imagem-geometrica" />
+      <h1>Nome</h1>
+      <h3>Cargo / Empresa</h3>
+      <img className="Rectangle" src={Rectangle} alt="imagem-geometrica" />
+      <h1>Nome</h1>
+      <h3>Cargo / Empresa</h3>
+      <img className="Rectangle" src={Rectangle} alt="imagem-geometrica" />
+      <h1>Nome</h1>
+      <h3>Cargo / Empresa</h3>
+      <img className="Rectangle2" src={Rectangle2} alt="imagem-geometrica" />
+      <img className="Rectangle2" src={Rectangle2} alt="imagem-geometrica" />
+      <img className="Rectangle2" src={Rectangle2} alt="imagem-geometrica" />
+
+      <h2>AQUI</h2>
+    </div>
+  );
 }
-
