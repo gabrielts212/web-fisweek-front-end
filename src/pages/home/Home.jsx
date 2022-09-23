cb1c75bf9c00823d88f5bba306f11cbb4f32020b
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2022-09-23 11:54:39 -0300

    Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,14 +15,14 @@ export function Home() {
     return (
         <Container>
             <Row>
-                {/* <Col  xs={12} md={8}><RegistrationInfo /></Col>
-                <Col xs={12} md={4}><ApplicationForm /></Col> */}
+                <Col  xs={12} md={8}><RegistrationInfo /></Col>
+                <Col xs={12} md={4}><ApplicationForm /></Col>
             </Row>
             <Row>
                 <ConferenceBar />
             </Row>
             <Row>
-                <Col  xs={12} md={6}><Leaders/></Col>
+                {/* <Col  xs={12} md={6}><Leaders/></Col> */}
             </Row>
             <Row>
                 <Schedule />
