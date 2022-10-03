13b0121506abb8d05b3039b430e0155562cbf024
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2022-10-03 18:10:39 -0300

    Finished Front of Leaders

--- src/components/leaders/Leaders.jsx
+++ src/components/leaders/Leaders.jsx
@@ -4,23 +4,26 @@ import { LeaderData } from "../leaderdata/LeaderData";
 import { useState } from "react";
 
 export function Leaders() {
+  const [showLeaders, setShowLeaders] = useState(false)
 
   const handleButtonClick = () => {
-    // console.log('Clicou!')
+    setShowLeaders(true)
   }
 
   return (
     <div className="Leaders">
       <div className="leaderGroup">
-        <LeaderData />
-        <LeaderData />
-        <LeaderData />
-        <LeaderData />
-        <LeaderData />
-        <LeaderData />
+      <LeaderData />
+      <LeaderData />
+      <LeaderData />
+      <LeaderData />
+      <LeaderData />
+      <LeaderData />
+
+      { showLeaders ? <LeaderData /> : null }
       </div>
 
-      <button className="showLeaders" onClick={handleButtonClick}>Ver todos os líderes</button>
+      <button className="showLeaders" onClick={handleButtonClick} hidden={showLeaders}>Ver todos os líderes</button>
     </div>
   );
 }
