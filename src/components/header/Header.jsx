212c812ba3b5a53b986989a06a1cce946d034eeb
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2022-10-04 16:34:03 -0300

    hidden menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ export function Header() {
         <img class="headerFis" src={HeaderFis} alt="image header" />
         <LanguageSwitcher />
 
-        <button
+        {/* <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
@@ -21,8 +21,8 @@ export function Header() {
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
               <a className="nav-link" href="/Leaders">
@@ -50,7 +50,7 @@ export function Header() {
               </a>
             </li>
           </ul>
-        </div>
+        </div> */}
       </nav>
     </div>
   );
