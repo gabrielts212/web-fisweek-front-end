7d5224ca83d4de2dbd77ec2dcf4471d25de336f7
Gabriel <91156517+gabrielts212@users.noreply.github.com>
2022-11-07 09:09:16 -0300

    look here

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -40,13 +40,13 @@ export function Home() {
                <Header />
             </Row>
             <Row>
-                <Col xs={12} md={8}><RegistrationInfo /></Col>
-                <Col xs={12} md={4}><ApplicationForm /></Col>
+                {/* <Col xs={12} md={8}><RegistrationInfo /></Col>
+                <Col xs={12} md={4}><ApplicationForm /></Col> */}
             </Row>
-            {/* <Row>
+            <Row>
                 <Col xs={12} md={8}><RegistrationInfo /></Col>
                 <Col xs={12} md={4}><BroadcastPage /></Col>
-            </Row> */}
+            </Row>
             <Row>
                 <ConferenceBar />
             </Row>
